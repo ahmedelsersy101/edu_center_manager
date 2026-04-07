d4fc55a21ec29b35f719800317e6217d517e8dde
ahmedsersy10 <ahmedelsersy94@gmail.com>
2026-04-07 12:55:21 +0200

    Initial project setup: repository initialized and README added

---
+++ android/app/src/main/kotlin/com/example/edu_center_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.edu_center_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EduCenterManager());
+}
+
+class EduCenterManager extends StatelessWidget {
+  const EduCenterManager({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Center Manager',
+    );
+  }
+}
